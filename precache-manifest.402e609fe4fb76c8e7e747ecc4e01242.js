f050441febab4a5c51c42d42fc97d52f7fafb1ca
Paper Lee <paper@diing.com>
2020-04-08 12:47:59 -0700

    Updates

---
+++ precache-manifest.402e609fe4fb76c8e7e747ecc4e01242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b0dbad1468b8e07613f8c6f948bf99",
+    "url": "/vr-ui/index.html"
+  },
+  {
+    "revision": "6e6e499270fea42bcfde",
+    "url": "/vr-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e52ac1bb93333380380",
+    "url": "/vr-ui/static/js/2.545bc10b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vr-ui/static/js/2.545bc10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6e499270fea42bcfde",
+    "url": "/vr-ui/static/js/main.29365d25.chunk.js"
+  },
+  {
+    "revision": "f5bf2228ed10477416bf",
+    "url": "/vr-ui/static/js/runtime-main.66ec155f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vr-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
