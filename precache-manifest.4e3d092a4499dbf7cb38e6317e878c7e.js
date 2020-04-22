6f48482418c3dd1f1b31ae8a7bd6e052a7772b84
Paper Lee <paper@diing.com>
2020-04-22 11:11:43 -0700

    Updates

---
+++ precache-manifest.4e3d092a4499dbf7cb38e6317e878c7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bb5adee27e75916aa0f6924f002285",
+    "url": "/vr-ui/index.html"
+  },
+  {
+    "revision": "3c2f11039f3c0b7c75dd",
+    "url": "/vr-ui/static/css/main.ea98ccd8.chunk.css"
+  },
+  {
+    "revision": "3b6484ab032e9d832842",
+    "url": "/vr-ui/static/js/2.716ea982.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/vr-ui/static/js/2.716ea982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2f11039f3c0b7c75dd",
+    "url": "/vr-ui/static/js/main.29534477.chunk.js"
+  },
+  {
+    "revision": "f5bf2228ed10477416bf",
+    "url": "/vr-ui/static/js/runtime-main.66ec155f.js"
+  },
+  {
+    "revision": "a99556139e4ae5b1890a3b9b61a7b896",
+    "url": "/vr-ui/static/media/bg.a9955613.jpg"
+  },
+  {
+    "revision": "252dcc96796066b3a2e67b1c8beb72a8",
+    "url": "/vr-ui/static/media/handinstrument.252dcc96.png"
+  }
+]);
